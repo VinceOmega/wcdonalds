e89f1339801539b4a504795d9e4675df32e7a46d
Larry Stanfield <vince.omega@gmail.com>
2021-12-14 22:30:47 -0500

    initial

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>
+            Do Do Do Do Do! You're Enjoying This! | WcDonalds
+        </title>
+        <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="/bootstrap/css/boostrap-responsive.min.css" rel="stylesheet" type="text/css">
+        <link href="/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row" style="min-height: inherit;">
+                <div class="span1">
+                </div>
+                <div class="span8">
+                    <div class="row">
+                        <div class="span12">
+                            <div class="wcdga-logo-container wcdga-logo">
+                                <span class="wcdga-logo-txt">WcDonalds</span>
+                            </div>
+                            We're working on building an <span class="txt-yellow">AWESOME</span> platform right now.</br>
+                            <span class="txt-yellow">SIGN UP</span> below to follow our mailing list, so you can be the first to be here when we launch!
+
+                            <!-- Begin Mailchimp Signup Form -->
+                            <div id="mc_embed_signup">
+                                <form action="https://redrobotprogramming.us10.list-manage.com/subscribe/post?u=2317518c506ddebec958ef932&amp;id=23687f5f92" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                    <div id="mc_embed_signup_scroll">
+                                    <div class="mc-field-group">
+                                        <label for="mce-EMAIL" class="email-label">Email Address</label>
+                                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Enter Email Address Here">
+                                    </div>
+                                    <div id="mce-responses" class="clear">
+                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                        <div class="response" id="mce-success-response" style="display:none"></div>
+                                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2317518c506ddebec958ef932_23687f5f92" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                        <!--End mc_embed_signup-->
+                </div>
+                <div class="span1">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
